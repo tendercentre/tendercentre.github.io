fa5557aa58140beac69ab40166eeb787f1bae079
Luke Bennett <hello@lukebennett.com.au>
2020-11-14 14:25:56 +1100

    Start fresh

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,13 @@
+module.exports = [{
+      plugin: require('/Users/luke/Sites/Websites/_archive/Port Macquarie Tender Centre/pmtc.com.au/node_modules/gatsby-plugin-canonical-urls/gatsby-browser.js'),
+      options: {"plugins":[],"siteUrl":"https://www.pmtc.com.au"},
+    },{
+      plugin: require('/Users/luke/Sites/Websites/_archive/Port Macquarie Tender Centre/pmtc.com.au/node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
+      options: {"plugins":[],"name":"gatsby-starter-tailwindcss","short_name":"starter","start_url":"/","background_color":"#3a5a78","theme_color":"#3a5a78","display":"minimal-ui","icon":"src/images/favicon.png"},
+    },{
+      plugin: require('/Users/luke/Sites/Websites/_archive/Port Macquarie Tender Centre/pmtc.com.au/node_modules/gatsby-plugin-google-analytics/gatsby-browser.js'),
+      options: {"plugins":[],"trackingId":"UA-111452383-1"},
+    },{
+      plugin: require('/Users/luke/Sites/Websites/_archive/Port Macquarie Tender Centre/pmtc.com.au/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
---
+++ .cache/async-requires.js
@@ -0,0 +1,9 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("/Users/luke/Sites/Websites/_archive/Port Macquarie Tender Centre/pmtc.com.au/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-404-js": () => import("/Users/luke/Sites/Websites/_archive/Port Macquarie Tender Centre/pmtc.com.au/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-index-js": () => import("/Users/luke/Sites/Websites/_archive/Port Macquarie Tender Centre/pmtc.com.au/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
+}
+
